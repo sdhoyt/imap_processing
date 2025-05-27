ed706a3e35a4532ada30f53884579fec13bac45f
Sean Hoyt <sean.hoyt@lasp.colorado.edu>
2025-05-27 15:14:30 -0600

    updated l1b cli

--- imap_processing/cli.py
+++ imap_processing/cli.py
@@ -952,13 +952,11 @@ class Lo(ProcessInstrument):
 
         elif self.data_level == "l1b":
             data_dict = {}
-            # TODO: Check this and update with new features as needed.
-            for input_type in dependencies.processing_input:
-                science_files = dependencies.get_file_paths(
-                    source="lo", descriptor=input_type.descriptor
-                )
-                dataset = load_cdf(science_files[0])
+            science_files = dependencies.get_file_paths(source="lo", data_type="l1a")
+            for file in science_files:
+                dataset = load_cdf(file)
                 data_dict[dataset.attrs["Logical_source"]] = dataset
+
             datasets = lo_l1b.lo_l1b(data_dict)
 
         elif self.data_level == "l1c":
